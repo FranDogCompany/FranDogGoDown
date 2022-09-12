f8d459fd4c118e801931910259c5cb08ace6bddf
FranDog <frandog1129@hotmail.com>
2022-09-13 00:04:21 +0800

    新增EPPlus插件

---
+++ Assets/Scripts/EnemyStatus.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStatus : MonoBehaviour
+{
+    public int id { get; set; }
+
+    public string enemyName { get; set; }
+
+    public int HP { get; set; }
+
+    public int ATK { get; set; }
+}
